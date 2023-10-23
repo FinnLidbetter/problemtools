a32058befb93ee04b9e64ae6fd4139966c419b7e
Finn Lidbetter <finnlidbetter@gmail.com>
2023-10-22 22:28:52 -0400

    Build support for ARM and viva 2.2

--- admin/update_docker.sh
+++ admin/update_docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+KOTLIN_VERSION=1.7.21
+
 TAG=develop
 UPDATE_LATEST=false
 if [ "$1" != "" ]; then
@@ -14,38 +16,43 @@ set -x
 
 # Make the build image and extract build artifacts
 # ===============================================
-sudo docker build \
+docker buildx build \
      -f Dockerfile.build \
-     -t problemtools/build:${TAG} \
+     -t finnlidbetter/problemtools-arm-build:${TAG} \
      --no-cache \
      --build-arg PROBLEMTOOLS_VERSION="${TAG}" \
      .
 mkdir -p artifacts
 rm -rf artifacts/deb/*
-sudo docker run --rm -v "$(pwd)/artifacts/:/artifacts" problemtools/build:${TAG} cp -r /usr/local/problemtools_build/deb /artifacts
-sudo chown -R $USER:$USER artifacts/
+docker run --rm -v "$(pwd)/artifacts/:/artifacts" finnlidbetter/problemtools-arm-build:${TAG} cp -r /usr/local/problemtools_build/deb /artifacts
+sudo chown -R $(id -u $USER):$(id -g $USER) artifacts/
+
+# Get Kotlin since it is not available through apt
+# ===============================================
+mkdir -p artifacts/kotlin
+curl -L -o artifacts/kotlin/kotlinc.zip https://github.com/JetBrains/kotlin/releases/download/v${KOTLIN_VERSION}/kotlin-compiler-${KOTLIN_VERSION}.zip
 
 
 # Build the actual problemtools images
 # ===============================================
 for IMAGE in minimal icpc full; do
-    sudo docker build\
+    docker buildx build\
          -f Dockerfile.${IMAGE}\
-         -t problemtools/${IMAGE}:${TAG}\
+         -t finnlidbetter/problemtools-arm-${IMAGE}:${TAG}\
          --build-arg PROBLEMTOOLS_VERSION=${TAG}\
          .
     if [ "$UPDATE_LATEST" = "true" ]; then
-        sudo docker tag problemtools/${IMAGE}:${TAG} problemtools/${IMAGE}:latest
+        docker tag finnlidbetter/problemtools-arm-${IMAGE}:${TAG} finnlidbetter/problemtools-arm-${IMAGE}:latest
     fi
 done
 
 
 # Push to Docker Hub
 # ===============================================
-sudo docker login
+docker login
 for IMAGE in minimal icpc full; do
-    sudo docker push problemtools/${IMAGE}:${TAG}
+    docker push finnlidbetter/problemtools-arm-${IMAGE}:${TAG}
     if [ "$UPDATE_LATEST" = "true" ]; then
-        sudo docker push problemtools/${IMAGE}:latest
+        docker push finnlidbetter/problemtools-arm-${IMAGE}:latest
     fi
 done
